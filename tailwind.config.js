f275989e75639324f3bbbbfd65e94de8b59c57d2
masayaShinoda <masayashida@gmail.com>
2024-04-08 17:38:33 +0700

    init project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    fontFamily: {
+      'sans': ['Montserrat', 'ui-sans-serif', 'system-ui'],
+    },
+    colors: {
+      transparent: 'red'
+    }
+  },
+  plugins: [],
+}
+
