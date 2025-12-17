0be936f3c1dfbc7bac1c70f8a3dc3fa0dee7c185
masayaShinoda <masayashida@gmail.com>
2025-12-17 16:09:07 +0700

    wip working pagination

--- products/views.py
+++ products/views.py
@@ -1,25 +1,42 @@
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.decorators.http import require_http_methods
+from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
-@require_http_methods(['GET'])
+@require_http_methods(['GET', 'POST'])
 def products(request):
-    products = Product.objects.all().order_by('-date_modified')
+    is_htmx_request = request.headers.get('HX-Request')
+
+    products_qs = Product.objects.all().order_by('-date_modified')
+    per_page = 1
 
     # paginator logic
-    paginator = Paginator(products, 2)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
+    paginator = Paginator(products_qs, per_page)
+    page_number = request.GET.get('page', 1)  # 1 by default
+
+    try:
+        page_obj = paginator.page(page_number)
+    except PageNotAnInteger:
+        # if page is not an int, return first page
+        page_obj = paginator.page(1)
+    except EmptyPage:
+        # if HTMX request and page number is out of range, return empty content
+        if is_htmx_request:
+            return HttpResponse("")
+        # if standard browser request, return last page
+        page_obj = paginator.page(paginator.num_pages)
 
     context = {
+        'products': page_obj.object_list,
         'page_obj': page_obj,
+        'is_htmx_request': is_htmx_request,
     }
 
     # If the request comes from HTMX, return only the product grid HTML.
-    if request.headers.get('HX-Request'):
-        template = 'partials/products_list.html'
+    if is_htmx_request:
+        template = 'partials/_products_list.html'
     else:
         template = 'pages/products.html'
 
@@ -27,9 +44,12 @@ def products(request):
     return render(request, template, context)
 
 
-@require_http_methods(['POST'])
-def product_details(request, product_slug):
+@require_http_methods(['GET'])
+def product(request, product_slug):
     product = get_object_or_404(Product, slug=product_slug)
-    # TODO
 
-    return 0
+    context = {
+        'product': product
+    }
+
+    return render(request, 'pages/product.html', context)
